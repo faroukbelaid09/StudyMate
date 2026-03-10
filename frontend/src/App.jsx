b75f7affa5e176ebe6bafcfca8f51afdb3d36e31
faroukbelaid09 <faroukbelaid9@gmail.com>
2026-03-10 15:25:11 +0300

    refactore(frontend): use env for API base URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,41 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Lecture from "./pages/Lecture";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
 
-        <Route path="/" element={<Dashboard />} />
+      <Navbar />
+
+      <Routes>
 
         <Route path="/login" element={<Login />} />
 
         <Route path="/register" element={<Register />} />
 
-        <Route path="/lecture/:id" element={<Lecture />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/lecture/:id"
+          element={
+            <ProtectedRoute>
+              <Lecture />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
